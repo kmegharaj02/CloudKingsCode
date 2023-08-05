77687d00866bc709ebc7bd02aa3da62311ab7460
kmegharaj02 <141474861+kmegharaj02@users.noreply.github.com>
2023-08-05 10:53:31 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,21 @@
+package coreJavaHarry;
+
+import java.util.Scanner;
+public class Percentage {
+    public static void main(String[] args) {
+        System.out.println("enter the marks");
+        Scanner sc = new Scanner(System.in);
+
+        int sub1 = sc.nextInt();
+        int sub2 = sc.nextInt();
+        int sub3 = sc.nextInt();
+        int sub4 = sc.nextInt();
+        int sub5 = sc.nextInt();
+        long sum = sub1 + sub2 + sub3 + sub4 + sub5;
+        long percentage = (sum/500)*100;
+        float cgpa=sum/50;
+        System.out.println(sum);
+        System.out.println(percentage);
+        System.out.println(cgpa);
+}
+}
